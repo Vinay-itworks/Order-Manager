0c81c9f2f774ecdfb1c770e85543a046f9472058
Vinay@itworks <vinay.poul@infotech.works>
2024-09-19 19:16:10 +0530

    Referenced Address to the User model and add edit, delete functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_19_105619) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_19_113953) do
+  create_table "addresses", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "address_type"
+    t.text "address"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.integer "zipcode"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +39,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_19_105619) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "addresses", "users"
 end
