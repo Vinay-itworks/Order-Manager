8560a16534e5b703e0526b02f57981de0ea93b71
Vinay@itworks <vinay.poul@infotech.works>
2024-09-20 13:29:48 +0530

    Worked with navbar and route and migrated product.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users, controllers: {
     sessions: "user/sessions",
     registrations: "user/registrations"
   }
   namespace :user do
+    get "about", to: "user#about", as: :about
     resources :addresses
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
